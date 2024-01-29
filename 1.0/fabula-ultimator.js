4ae395fca2da6ee507aacccb3cdc39b766cffaea
morgdalaine <morgdalaine@gmail.com>
2024-01-28 19:38:52 -0600

    Begin --export

--- 1.0/fabula-ultimator.js
+++ 1.0/fabula-ultimator.js
@@ -5,12 +5,17 @@ var FabulaUltimator =
       api: '!fabula',
       script: 'FabulaUltimator',
       version: '0.0',
+      journalUrl: 'https://journal.roll20.net/character/',
+      assetUrl: 'https://raw.githubusercontent.com/morgdalaine/fabula-ultimator/main/assets/',
       commands: {
         help: `!fabula --help`,
         import: `!fabula --import`,
         export: `!fabula --export`,
         config: `!fabula --config`,
         reset: `!fabula --reset`,
+      },
+      assets: {
+        feather: 'feather.png',
       },
     };
 
@@ -53,6 +58,16 @@ var FabulaUltimator =
       const [flag, params] = args;
       log({ this: 'eventHandler', message: message.content, flag, params, command, player });
       switch (flag) {
+        case 'export':
+          if (params) {
+            exportSheet(player, params);
+            return;
+          }
+
+          listSheetsForExport(player);
+
+          break;
+
         case 'help':
         default:
           sendHelpMenu(player);
@@ -60,7 +75,7 @@ var FabulaUltimator =
     };
 
     const sendHelpMenu = (player) => {
-      const helpBlueprint = {
+      const blueprint = {
         header: makeHeader({
           text: `FabulaUltimator ${G_CONSTANTS.version}.${Date.now()}`,
         }),
@@ -71,24 +86,34 @@ var FabulaUltimator =
           }),
 
           // --import
-          makeLink({ text: '!fabula --import json' }),
+          `<div style="margin-bottom: 8px;">`,
+          makeLink({ text: '!fabula --import json', href: '!fabula' }),
           makeSpan({ text: 'Import JSON to sheet' }),
+          `</div>`,
 
           // --export
-          makeLink({ text: '!fabula --export [sheetname]' }),
+          `<div style="margin-bottom: 8px;">`,
+          makeLink({ text: '!fabula --export [sheetname]', href: '!fabula --export' }),
           makeSpan({ text: 'List sheets to export to JSON, export sheet if provided <sheetname>' }),
+          `</div>`,
 
           // --config
+          `<div style="margin-bottom: 8px;">`,
           makeLink({ text: '!fabula --config' }),
           makeSpan({ text: 'View sheet import options' }),
+          `</div>`,
 
           // --reset
+          `<div style="margin-bottom: 8px;">`,
           makeLink({ text: '!fabula --reset' }),
           makeSpan({ text: 'Reset player options to defaults' }),
+          `</div>`,
 
           // --help
+          `<div style="">`,
           makeLink({ text: '!fabula --help' }),
           makeSpan({ text: 'Show this menu' }),
+          `</div>`,
         ],
       };
 
@@ -97,28 +122,111 @@ var FabulaUltimator =
         // TBA
       }
 
-      log(helpBlueprint);
-      sendChat(G_CONSTANTS.script, fabulaContainer(helpBlueprint), null, {
+      sendChat(G_CONSTANTS.script, fabulaContainer(blueprint), null, { noarchive: true });
+    };
+
+    const listSheetsForExport = (player) => {
+      const sheets = findObjs({
+        _type: 'character',
+      });
+
+      const blueprint = {
+        header: makeHeader({ text: 'Sheet Export' }),
+        body: [
+          makeSpan({
+            text:
+              'Click to download icon to export JSON<br />' + 'Click sheet name to open journal',
+            style: STYLE_SUBTITLE,
+          }),
+        ],
+      };
+
+      sheets.forEach((sheet) => {
+        if (player && !playerIsGM(player.id)) {
+        }
+
+        blueprint.body.push(createSheetEntry(sheet));
+      });
+
+      whisper(player, fabulaContainer(blueprint));
+    };
+
+    const exportSheet = (player, params) => {
+      log(`exportSheet > ${params}`);
+    };
+
+    const STYLE_DOWNLOAD_LINK = [
+      `position: relative;`,
+      `background: transparent;`,
+      `border: 1px solid transparent;`,
+      `border-radius: 4px;`,
+      `padding: 0px;`,
+      `width: 40px;`,
+      `height: 40px;`,
+    ].join('');
+    const createSheetEntry = (sheet) => {
+      const sheetName = sheet.get('name');
+
+      const journal = makeJournalLink(sheet);
+      const feather = makeImage({
+        src: getAsset('feather'),
+        alt: `Download ${sheetName}`,
+        style: STYLE_IMAGE,
+        width: 20,
+        height: 20,
+      });
+      const download = makeLink({
+        text: feather,
+        href: `!fabula --export ${sheetName}`,
+        style: STYLE_DOWNLOAD_LINK,
+        title: `Download ${sheetName}`,
+      });
+
+      const sheetEntry = [
+        `<table style="width: 100%">`,
+        `<tr>`,
+        `<td>`,
+        journal,
+        `</td>`,
+        `<td style="float: right;">`,
+        download,
+        `</td>`,
+        `</tr>`,
+        `</table>`,
+      ];
+
+      return sheetEntry.join('');
+    };
+
+    const whisper = (player, message) => {
+      const whisperTo = player ? `/w ${player.get('displayname')} ` : '';
+      sendChat(G_CONSTANTS.script, `${whisperTo}${message}`, null, {
         noarchive: true,
       });
     };
 
+    const getAsset = (asset) => G_CONSTANTS.assetUrl + G_CONSTANTS.assets[asset];
+
+    const STYLE_CONTAINER = [
+      `background: white;`,
+      `border-radius: 4px;`,
+      `box-shadow: rgba(0, 0, 0, 0.2) 0px 2px 1px -1px, rgba(0, 0, 0, 0.14) 0px 1px 1px 0px, rgba(0, 0, 0, 0.12) 0px 1px 3px 0px;`,
+    ].join('');
     const fabulaContainer = (blueprint) => {
       return [
-        `<div style="background: white; border-radius: 4px;">`,
-        `<div>`,
-        blueprint.header,
-        `</div>`,
+        `<div style="${STYLE_CONTAINER}">`,
+        blueprint.header ? `<div>${blueprint.header}</div>` : '',
         `<div style="padding: 8px;">`,
-        ...blueprint.body,
+        `${blueprint.body.join('')}`,
         `</div>`,
         `</div>`,
       ].join('');
     };
 
-    const FONT_TITLE = `font-family: 'Antonio', sans-serif;`;
+    const FONT_TITLE = `font-family: Antonio, sans-serif;`;
     const FONT_BODY = `font-family: 'PT Sans Narrow', sans-serif;`;
     const FONT_MONOSPACE = `font-family: Consolas, monospace;`;
+
     const STYLE_HEADER = [
       `display: block;`,
       FONT_TITLE,
@@ -131,7 +239,7 @@ var FabulaUltimator =
       `color: rgb(255, 255, 255);`,
       `background: rgb(103, 65, 104);`,
       `border-radius: 4px 4px 0 0;`,
-    ].join(' ');
+    ].join('');
     const makeHeader = ({ text, tag = 'h3', style = STYLE_HEADER }) => {
       return makeSpan({ tag, text, style });
     };
@@ -148,18 +256,70 @@ var FabulaUltimator =
       return `<${tag} style="${style}">${text}</${tag}>`;
     };
 
-    const STYLE_LINK = [`display: block;`, FONT_MONOSPACE, `background: rgb(110, 70, 141);`].join(
-      ''
-    );
-    const makeLink = ({ text, href = text, style = STYLE_LINK }) => {
-      return `<a style="${style}" href="${href}">${text}</a>`;
+    const STYLE_LINK = [
+      `display: block;`,
+      FONT_MONOSPACE,
+      `background: rgb(110, 70, 141);`,
+      `padding: 4px;`,
+      `color: white;`,
+      `border: none;`,
+    ].join('');
+    const makeLink = ({ text, href = text, style = STYLE_LINK, title = text }) => {
+      return `<a style="${style}" href="${href}" title="${title}">${text}</a>`;
+    };
+
+    const STYLE_JOURNAL_LINK = [
+      `display: block;`,
+      FONT_BODY,
+      `background: transparent;`,
+      `padding :4px;`,
+      `color: black;`,
+      `border: none;`,
+    ].join('');
+    const STYLE_JOURNAL_IMAGE = [`border: 1px solid #000000;`, `border-radius: 4px;`].join('');
+    const STYLE_JOURNAL_TEXT = [FONT_TITLE, `text-transform: uppercase;`, `padding: 8px;`].join('');
+    const makeJournalLink = (sheet) => {
+      const avatar = sheet.get('avatar');
+      const sheetName = sheet.get('name');
+      const blueprint = [
+        `<table>`,
+        `<tr>`,
+        `<td>`,
+        makeImage({ src: avatar, style: STYLE_JOURNAL_IMAGE, alt: sheetName }),
+        `</td>`,
+        `<td>`,
+        `<span style="${STYLE_JOURNAL_TEXT}">${sheetName}</span>`,
+        `</td>`,
+        `</tr>`,
+        `</table>`,
+      ];
+
+      return makeLink({
+        text: blueprint.join(''),
+        href: G_CONSTANTS.journalUrl + sheet.get('id'),
+        style: STYLE_JOURNAL_LINK,
+        title: `Open ${sheetName}`,
+      });
+    };
+
+    const STYLE_IMAGE = [`position: absolute;`, `top: 25%;`, `left: 25%;`].join('');
+    const makeImage = ({ src, alt, width = 40, height = 40, style = STYLE_IMAGE }) => {
+      return [
+        `<img `,
+        `style="${style}"`,
+        `src="${src}"`,
+        `alt="${alt}"`,
+        `width="${width}"`,
+        `height="${height}"`,
+        `>`,
+      ].join('');
     };
 
     on('ready', () => {
       FabulaUltimator.checkInstall();
       FabulaUltimator.registerEventHandlers();
       log(`FabulaUltimator ${G_CONSTANTS.version}.${Date.now()}`);
-      sendChat('System', '!fabula --help');
+      sendChat('System', '!fabula');
     });
 
     return {
